678bf6961405f96fd27f602681511005b6267846
MiguelJiMoral <migueljimenezmra@gmail.com>
2024-03-27 17:36:39 -0600

    Se agregaron archivos para el funcionamiento del backend

--- TareasLista/src/main/java/com/mijim/usuarios/tareaslista/Service/impl/UserIMPL.java
+++ TareasLista/src/main/java/com/mijim/usuarios/tareaslista/Service/impl/UserIMPL.java
@@ -5,7 +5,10 @@ import com.mijim.usuarios.tareaslista.Entity.User;
 import com.mijim.usuarios.tareaslista.Repo.UserRepo;
 import com.mijim.usuarios.tareaslista.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 @Service
